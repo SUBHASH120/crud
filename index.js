7cf60aaa0410b7fd5c10a79e20cca11c0c4fd26f
SUBHASH120 <subhash.kesarwani383@gmail.com>
2024-09-18 18:51:41 +0530

    removed unwanted code

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
-
-// app.get('/',(req,res)=>{
-//     res.send('Hello world');
-// })
 app.use('/',customers);
 
 app.listen(port,()=>{
