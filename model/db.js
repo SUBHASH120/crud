feda3062bfe013e3b9602ee1e79290c143e41570
SUBHASH120 <subhash.kesarwani383@gmail.com>
2024-09-19 11:56:43 +0530

    change in db file

--- model/db.js
+++ model/db.js
@@ -2,11 +2,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: '13.201.193.71',
-    database: 'demo',
-    password: 'password',
-    port: 5432
+    user: process.env.PGUSER || 'postgres',
+    host: process.env.PGHOST || 'localhost',
+    database: process.env.PGDATABASE || 'demo',
+    password: process.env.PGPASSWORD || 'password',
+    port: process.env.PGPORT || 5432,
 })
 if(pool.connect()){
     console.log('connected')
